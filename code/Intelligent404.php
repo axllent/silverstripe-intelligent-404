b3eccf974b4d4a4b0c0286fd50c2abbc6b268140
helpfulrobot <chris@silverstripe.com>
2015-12-20 16:33:52 +1300

    Converted to PSR-2

--- code/Intelligent404.php
+++ code/Intelligent404.php
@@ -14,91 +14,84 @@
  * Authors: Techno Joy development team (www.technojoy.co.nz)
  */
 
-class Intelligent404 extends Extension {
+class Intelligent404 extends Extension
+{
 
-	public function onAfterInit() {
+    public function onAfterInit()
+    {
+        if (!Director::isDev()) { // Only on live site
 
-		if (!Director::isDev()) { // Only on live site
+            $errorcode = $this->owner->failover->ErrorCode ? $this->owner->failover->ErrorCode : 404;
 
-			$errorcode = $this->owner->failover->ErrorCode ? $this->owner->failover->ErrorCode : 404;
+            $extract = preg_match('/^([a-z0-9\.\_\-\/]+)/i', $_SERVER['REQUEST_URI'], $rawString);
 
-			$extract = preg_match('/^([a-z0-9\.\_\-\/]+)/i', $_SERVER['REQUEST_URI'], $rawString);
+            if ($errorcode == 404 && $extract) {
+                $uri = preg_replace('/\.(aspx?|html?|php[34]?)$/i', '', $rawString[0]);
+                $parts = preg_split('/\//', $uri, -1, PREG_SPLIT_NO_EMPTY);
+                $page_key = array_pop($parts);
+                $sounds_like = soundex($page_key);
 
-			if ($errorcode == 404 && $extract) {
-				$uri = preg_replace('/\.(aspx?|html?|php[34]?)$/i', '', $rawString[0]);
-				$parts = preg_split('/\//', $uri, -1, PREG_SPLIT_NO_EMPTY);
-				$page_key = array_pop($parts);
-				$sounds_like = soundex($page_key);
+                // extend ignored classes with child classes
+                $ignoreClassNames = array();
+                if ($configClasses = Config::inst()->get('Intelligent404', 'intelligent_404_ignored_classes')) {
+                    foreach ($configClasses as $class) {
+                        $ignoreClassNames = array_merge($ignoreClassNames, array_values(ClassInfo::subclassesFor($class)));
+                    }
+                }
 
-				// extend ignored classes with child classes
-				$ignoreClassNames = array();
-				if ($configClasses = Config::inst()->get('Intelligent404', 'intelligent_404_ignored_classes')) {
-					foreach ($configClasses as $class) {
-						$ignoreClassNames = array_merge($ignoreClassNames, array_values(ClassInfo::subclassesFor($class)));
-					}
-				}
+                // get all pages
+                $SiteTree = SiteTree::get()->exclude('ClassName', $ignoreClassNames);
 
-				// get all pages
-				$SiteTree = SiteTree::get()->exclude('ClassName', $ignoreClassNames);
+                // Translatable support
+                if (class_exists('Translatable')) {
+                    $SiteTree = $SiteTree->filter('Locale', Translatable::get_current_locale());
+                }
 
-				// Translatable support
-				if (class_exists('Translatable')) {
-					$SiteTree = $SiteTree->filter('Locale', Translatable::get_current_locale());
-				}
+                // Multisites support
+                if (class_exists('Multisites')) {
+                    $SiteTree = $SiteTree->filter('SiteID', Multisites::inst()->getCurrentSiteId());
+                }
 
-				// Multisites support
-				if (class_exists('Multisites')) {
-					$SiteTree = $SiteTree->filter('SiteID', Multisites::inst()->getCurrentSiteId());
-				}
+                $ExactMatches = new ArrayList();
+                $PossibleMatches = new ArrayList();
 
-				$ExactMatches = new ArrayList();
-				$PossibleMatches = new ArrayList();
+                foreach ($SiteTree as $page) {
+                    if ($page->URLSegment == $page_key) {
+                        $ExactMatches->push($page);
+                    } elseif ($sounds_like == soundex($page->URLSegment)) {
+                        $PossibleMatches->push($page);
+                    }
+                }
 
-				foreach ($SiteTree as $page) {
-					if ($page->URLSegment == $page_key) {
-						$ExactMatches->push($page);
-					}
-					elseif ($sounds_like == soundex($page->URLSegment)) {
-						$PossibleMatches->push($page);
-					}
-				}
+                $ExactCount = $ExactMatches->Count();
+                $PossibleCount = $PossibleMatches->Count();
 
-				$ExactCount = $ExactMatches->Count();
-				$PossibleCount = $PossibleMatches->Count();
+                if ($ExactCount == 1) {
+                    return $this->RedirectToPage($ExactMatches->First()->Link());
+                } elseif ($ExactCount == 0 && $PossibleCount == 1) {
+                    return $this->RedirectToPage($PossibleMatches->First()->Link());
+                } elseif ($ExactCount > 1 || $PossibleCount > 1) {
+                    $ExactMatches->merge($PossibleMatches);
+                    $content = $this->owner->customise(array(
+                        'Pages' => $ExactMatches
+                    ))->renderWith(
+                        array('Intelligent404Options')
+                    );
+                    $this->owner->Content .= $content;
+                }
+            }
+        }
+    }
 
-				if ($ExactCount == 1) {
-					return $this->RedirectToPage($ExactMatches->First()->Link());
-				}
-
-				elseif ($ExactCount == 0 && $PossibleCount == 1) {
-					return $this->RedirectToPage($PossibleMatches->First()->Link());
-				}
-
-				elseif ($ExactCount > 1 || $PossibleCount > 1) {
-					$ExactMatches->merge($PossibleMatches);
-					$content = $this->owner->customise(array(
-					    'Pages' => $ExactMatches
-					))->renderWith(
-					    array('Intelligent404Options')
-					);
-					$this->owner->Content .= $content;
-				}
-
-			}
-
-		}
-
-	}
-
-	/*
-	 * Internal redirect function
-	 * @param string
-	 * @return 301 response / redirect
-	 */
-	public function RedirectToPage($url) {
-		$response = new SS_HTTPResponse();
-		$response->redirect($url, 301);
-		throw new SS_HTTPResponse_Exception($response);
-	}
-
+    /*
+     * Internal redirect function
+     * @param string
+     * @return 301 response / redirect
+     */
+    public function RedirectToPage($url)
+    {
+        $response = new SS_HTTPResponse();
+        $response->redirect($url, 301);
+        throw new SS_HTTPResponse_Exception($response);
+    }
 }
